403a741e11937c9181162b7b8db01944b0fe6006
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-11-11 15:33:09 -0300

    Configurando o ip da maquina server.py

---
+++ bdd.java
@@ -0,0 +1,5 @@
+
+Cenário: Acessar e visualizar as informações do perfil do usuário
+Dado que o usuário está logado em sua conta
+Quando ele acessa a seção "Perfil"
+Então ele vê suas informações pessoais como nome, email e endereço, e opções para editar os dados
