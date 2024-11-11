ea49aadc20dd4c8be7c7d18ad31e201932dc5efb
IagoraNz <iago.almeida@ufpi.edu.br>
2024-11-11 15:39:29 -0300

    Remove bdd.java file containing user profile access scenario

--- bdd.java
+++
@@ -1,5 +0,0 @@
-
-Cenário: Acessar e visualizar as informações do perfil do usuário
-Dado que o usuário está logado em sua conta
-Quando ele acessa a seção "Perfil"
-Então ele vê suas informações pessoais como nome, email e endereço, e opções para editar os dados
