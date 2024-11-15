1826f3743fd48a772a59612e25ccc9cd942792da
Daniel Rodrigues <capau.2020120iipi0064@aluno.ifpi.edu.br>
2024-11-15 17:05:51 -0300

    Testes de server finalizado

--- test_server.py
+++ test_server.py
@@ -46,3 +46,29 @@ def test_shutdown_server(setup_server):
     control_client.connect(('localhost', TEST_PORT))
     control_client.send("shutdown_server".encode())  # Envia o comando especial
     control_client.close()
+
+def test_client_disconnect(setup_server):
+    """Teste de desconexão do cliente."""
+    client = create_client("cliente")
+    client.send("Teste de mensagem do cliente".encode())
+    client.send("concluido1234".encode())  # Comando para encerrar conexão
+    response = client.recv(1024).decode()
+    assert "[Erro] Nenhum gerente disponível para TestArea no momento." in response
+    client.close()
+
+def test_manager_disconnect(setup_server):
+    """Teste de desconexão do gerente."""
+    manager = create_client("gerente")
+    manager.send("Teste inicial do gerente".encode())
+    manager.send("concluido1234".encode())  # Comando para encerrar conexão
+    response = manager.recv(1024).decode()
+    assert "" in response
+    manager.close()
+
+def test_no_manager_available(setup_server):
+    """Teste de mensagem de erro quando não há gerente disponível."""
+    client = create_client("cliente")
+    client.send("Mensagem sem gerente disponível".encode())
+    response = client.recv(1024).decode()
+    assert "[Erro] Nenhum gerente disponível" in response
+    client.close()
